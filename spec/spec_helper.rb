7fbde882aa5f5335f58c7dde955bd1b9ea65ceeb
Niklaus Giger <niklaus.giger@member.fsf.org>
2014-10-03 23:46:52 +0200

    Allow failures for STRICT_VARIABLES

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 RSpec.configure do |c|
   c.default_facts = {
-    :operatingsystem => 'Debian',
+    :operatingsystem        => 'Debian',
+    :osfamily               => 'Debian',
+    :operatingsystemrelease => 'wheezy',
+    :lsbdistcodename        => 'wheezy',
+    :lsbdistid              => 'debian',
       # for concat/manifests/init.pp:193
-    :id => 'id',
-    :concat_basedir => '/opt/concat',
-    :path => '/path',
+    :id                     => 'id',
+    :concat_basedir         => '/opt/concat',
+    :path                   => '/path',
   }
 end
